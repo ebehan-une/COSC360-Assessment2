55e09bb4178ac8350a11867a90ac6e21ce303b11
ebehan-une <ebehan@myune.edu.au>
2026-08-16 16:13:10 +1000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect('/admin');
+})->middleware('auth');;
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
